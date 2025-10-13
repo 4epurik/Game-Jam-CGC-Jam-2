4e9b99cdfeaccc329337025bb76943907acb338d
Alex.A.Om.temp <Alex.A.Om.temp@gmail.com>
2025-10-13 21:25:24 +0300

    refolder + minor player fix. + double jump + dash

--- Assets/Script/ReloadGame.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-// ScriptableObject для хранения данных
-[CreateAssetMenu(fileName = "ReloadGame", menuName = "Game/ReloadGame")]
-public class ReloadGame : ScriptableObject
-{
-    public bool needReloadGame;
-}
